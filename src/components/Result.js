e6cb230205627973e73b7f02a7b99ca7b148c11f
venkatasaikirana <venkatasaikira@planetpinnacle.com>
2023-05-30 14:29:46 -0400

    added-hooks-result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useSelector } from 'react-redux';
 
 const Result = () => {
     const answers = useSelector(state => state.answers);
+    const [score, setScore] = useState(0);
 
+    useEffect(() => {
     const correctAnswers = answers.filter(answer => answer.isCorrect);
+    setScore(correctAnswers.length);
+    }, [answers]);
 
     return (
         <div className="result">
             <h2>Result</h2>
-            <p>You answered {correctAnswers.length} out of 10 questions Correctly!!!</p>
+            <p>You answered {score} out of 10 questions Correctly!!!</p>
         </div>
     );
 
