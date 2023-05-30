3bc3279f6645e450575197b23b4f65cf85ab73a6
venkatasaikirana <venkatasaikira@planetpinnacle.com>
2023-05-30 14:27:18 -0400

    used hooks

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { answerQuestion } from '../Redux/actions';
 
 const Question = ({ questionNumber, onNextQuestion }) => {
   const questions = useSelector(state => state.questions);
-  const answers = useSelector((state) => state.answers)
+  const answers = useSelector(state => state.answers);
   const dispatch = useDispatch();
   const currentQuestion = questions[questionNumber - 1];
+  const [score, setScore] = useState(0);
 
   const handleAnswer = (answer) => {
     dispatch(answerQuestion(questionNumber, answer));
     onNextQuestion();
   };
 
-  const getScore = () => {
+  useEffect(() => {
     const correctAnswers = answers.filter(answer => answer.isCorrect);
-    return correctAnswers.length;
-  };
+    setScore(correctAnswers.length);
+  }, [answers]);
 
   return (
     <div className="question">
-      <div className="score">score: {getScore()}</div>
+      <div className="score">Score: {score}</div>
       <h2>Question {questionNumber}</h2>
       <p>{currentQuestion.question}</p>
       <div className="answers">
